e9e5bca579f7c82103332863dc2e5c6646e50ed2
azizsheq <azizsheq@gmail.com>
2021-04-02 13:21:48 +0800

    adding to orders complete

--- index.js
+++ index.js
@@ -21,18 +21,24 @@ const ObjectId = require('mongodb').ObjectId
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.l7yew.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 // console.log(uri);
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
+    // collection for products
     const productCollection = client.db("MyEasyMartDB").collection("products");
+    // collection for orders    
+    const orderCollection = client.db("MyEasyMartDB").collection("orders");
     console.log('connected with mongodb database')
 
+
     // data POST testing
-    // const newEvent = { "name": "Product One" };
-    // productCollection.insertOne(newEvent)
+    // const newEvent = { "name": "Order One" };
+    // orderCollection.insertOne(newEvent)
     //     .then(result => { console.log(`Successfully inserted item with _id: ${result.insertedId}`) })
     //     .catch(err => { console.error(`Failed to insert item: ${err}`) })
 
-    // adding new product
+
+    // adding new items to -> products
     app.post('/addProduct', (req, res) => {
         const newProduct = req.body;
         // console.log("New Product:", newProduct);
@@ -45,24 +51,52 @@ client.connect(err => {
     })
 
 
-    // getting products
+
+    // getting all items from -> products
     app.get('/getProducts', (req, res) => {
         productCollection.find()
-        .toArray((err, products) => {
-            res.send(products)
-        })
+            .toArray((err, products) => {
+                res.send(products)
+            })
     })
 
 
-    // delete uploaded product
+    // getting specific user clicked product from -> products
+    app.get('/getClickedProduct/:id', (req, res) => {
+        const id = ObjectId(req.params.id);
+        productCollection.findOne({ _id: id })
+            .then(documents => {
+                // console.log(documents);
+                res.send(documents);
+            })
+    })
+
+
+    // delete each selected item  from -> products
     app.delete('/deleteProduct/:id', (req, res) => {
         const id = ObjectId(req.params.id);
         // console.log('select for delete: ', id);
-        productCollection.findOneAndDelete({_id: id})
-        .then(documents => {
-            res.send(!!documents.value)
-        })
+        productCollection.findOneAndDelete({ _id: id })
+            .then(documents => {
+                res.send(!!documents.value)
+            })
     })
+
+
+    // adding new items to -> orders
+    app.post('/addToOrders', (req, res) => {
+        const newOrder = req.body;
+        // console.log("New Product:", newProduct);
+        orderCollection.insertOne(newOrder)
+            .then(result => {
+                console.log(`Successfully ordered item with _id: ${result.insertedId}`)
+                res.send(result.insertedId > 0)
+            })
+            .catch(err => { console.error(`Failed to order item: ${err}`) })
+    })
+
+
+    
     //end
 });
 
@@ -71,6 +105,8 @@ client.connect(err => {
 app.get('/', (req, res) => {
     res.send('Hello, My Easy Mart Server is Running !')
 })
+
+
 // default
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
